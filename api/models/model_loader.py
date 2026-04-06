95de0e7aa0e47f06ed3eb495bbf75d37e018db0c
inthila <ichanthi@charlotte.edu>
2026-04-05 20:52:32 -0400

    feat: added model loader

--- api/models/model_loader.py
+++ api/models/model_loader.py
@@ -1,11 +1,11 @@
-from . import orders, order_details, recipes, sandwiches, resources
-
+from . import ingredients, menu_item_ingredients, menu_items, order_items, orders, promo_codes, reviews
 from ..dependencies.database import engine
 
-
 def index():
+    ingredients.Base.metadata.create_all(engine)
+    menu_items.Base.metadata.create_all(engine)
+    menu_item_ingredients.Base.metadata.create_all(engine)
     orders.Base.metadata.create_all(engine)
-    order_details.Base.metadata.create_all(engine)
-    recipes.Base.metadata.create_all(engine)
-    sandwiches.Base.metadata.create_all(engine)
-    resources.Base.metadata.create_all(engine)
+    order_items.Base.metadata.create_all(engine)
+    promo_codes.Base.metadata.create_all(engine)
+    reviews.Base.metadata.create_all(engine)
