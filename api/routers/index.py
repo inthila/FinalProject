9ccc24a9b9bb4de9d1c9e73609e2abd613c56994
Jordan Sentosa <jrdn.sentosa@gmail.com>
2026-04-23 20:45:09 -0400

    feat: add routers and update index for all tables

--- api/routers/index.py
+++ api/routers/index.py
@@ -1,6 +1,11 @@
-from . import orders, order_details
+from . import ingredients, menu_item_ingredients, menu_items, order_items, orders, promo_codes, reviews
 
 
 def load_routes(app):
+    app.include_router(ingredients.router)
+    app.include_router(menu_item_ingredients.router)
+    app.include_router(menu_items.router)
+    app.include_router(order_items.router)
     app.include_router(orders.router)
-    app.include_router(order_details.router)
+    app.include_router(promo_codes.router)
+    app.include_router(reviews.router)
